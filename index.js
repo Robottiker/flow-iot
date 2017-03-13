dc6ae042103664106c088fe7e50a218c42dce0e4
Adrian Ottiker <adrian@ottiker.com>
2017-03-13 19:32:50 +0100

    make main file flow agnostic

--- index.js
+++ index.js
@@ -2,33 +2,28 @@
 
 const Transform = require("stream").Transform;
 
-exports.decode = (scope, state, args, data, stream, next) => {
-
-    const dec = new Transform({
-        objectMode: args.objectMode || true,
+exports.decode = (objectMode) => {
+    return new Transform({
+        objectMode: objectMode || true,
         transform: (chunk, enc, done) =>  {
+
             // ..decode chunk
-            chunk = JSON.parse(chunk.toString());
-            console.log('Flow-Iot.decode(chunk):', Object.keys(chunk));
+            chunk = chunk.toString();
+            console.log('Flow-Iot.decode(chunk):', chunk);
             done(null, chunk);
         }
     });
-
-    next(null, data, stream.pipe(dec));
 };
 
-exports.encode = (scope, state, args, data, stream, next) => {
-
-    const enc = new Transform({
-        objectMode: args.objectMode || true,
+exports.encode = (objectMode) => {
+    return new Transform({
+        objectMode: objectMode || true,
         transform: (chunk, enc, done) => {
+
             // ..encode chunk
-            chunk = JSON.stringify(chunk);
             console.log('Flow-Iot.encode(chunk):', chunk.length);
             //done(null, Buffer.from(JSON.stringify(chunk), 'utf8'));
             done(null, chunk);
         }
     });
-
-    next(null, data, stream.pipe(enc));
 };
